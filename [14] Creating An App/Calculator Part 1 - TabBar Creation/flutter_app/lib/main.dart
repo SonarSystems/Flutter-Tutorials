1265144568a91c378d8ca7524374acec4d252d6e
Sonar Systems <SonarSystems@users.noreply.github.com>
2019-03-27 12:13:41 +0000

    Update main.dart

--- [14] Creating An App/Calculator Part 1 - TabBar Creation/flutter_app/lib/main.dart
+++ [14] Creating An App/Calculator Part 1 - TabBar Creation/flutter_app/lib/main.dart
@@ -61,7 +61,7 @@ class _MyHomePageState extends State<MyHomePage> {
                         ]
                     ),
 
-                    title: Text("VENT")
+                    title: Text("Calculator")
                 ),
 
                 body: TabBarView(
